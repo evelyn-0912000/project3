ef82fc8cbf86e5a1cc6ed5670c5f5c97d054b659
evelyn-0912000 <evelyn.vdkmp@gmail.com>
2016-04-20 10:57:40 +0200

    Added abstraction button images

--- Meteen Rotterdam/Meteen Rotterdam/Game1.cs
+++ Meteen Rotterdam/Meteen Rotterdam/Game1.cs
@@ -133,7 +133,8 @@ namespace Meteen_Rotterdam
         //System.Console.WriteLine("OFFSET" + grabOffset);
         //System.Console.WriteLine("MOUSE" + mousePosition);
         //System.Console.WriteLine("MAP" + mapPosition);
-        
+        // dummy comment
+
       }
       if (mouseState.LeftButton == ButtonState.Released)
       {

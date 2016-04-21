f3211cc5cff72d3dd96865c7f39496e167ae5279
Decade1 <leonardvonkn@gmail.com>
2016-04-21 14:20:24 +0200

    pin2

---
+++ Meteen Rotterdam/Map.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Map
+{
+  Texture2D texture;
+  Vector2 position;
+
+  public Map(Vector2 position, Texture2D texture)
+  {
+    this.texture = texture;
+    //this.position = position;
+  }
+
+  public void UpdatePos(Vector2 position)
+  {
+    this.position = position;
+  }
+
+  public void Draw(SpriteBatch spriteBatch)
+  {
+    spriteBatch.Draw(texture, position, Color.White);
+  }
+
+}
